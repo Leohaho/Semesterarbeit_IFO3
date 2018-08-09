b74eba3c92b38513665169794c4f1e0573aaa2d6
Leo Hartmann <Leo.Hartmann@stud.hs21.de>
2018-08-09 19:43:29 +0200

    Laden Mitarbeiter und Abteilung beim start

--- Semesterarbeit_IFO3/MyForm.cpp
+++ Semesterarbeit_IFO3/MyForm.cpp
@@ -6,9 +6,12 @@ using namespace System::Windows::Forms;
 [STAThread]
 
 void main(array<String^>^ args) {
+	readma(MA);
+	readal(AT, AL);
+	save = 1;
+
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Semesterarbeit_IFO3::MyForm form;
 	Application::Run(%form);
-
 }
